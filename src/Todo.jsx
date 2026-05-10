1f7f03ff9b260853c2c866aab48a3787cbb675d8
tahmina-nishu <akthertahmina572@gmail.com>
2026-05-10 06:47:08 +0600

    Conditional rendering

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,5 +1,10 @@
-const Todo = ({task}) => {
-    return <li>Task : {task}</li>;
+const Todo = ({task, isDone}) => {
+    // Conditional rendering option 1:
+
+    if(isDone === true)
+        return <li>Finished : {task}</li>
+    else
+        return <li>Work on : {task}</li>
 };
 
 export default Todo;
